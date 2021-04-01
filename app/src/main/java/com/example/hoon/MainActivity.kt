fe65f7012ce0526042596be838b07e30f5b49938
atiwatprathep <77892874+atiwatprathep@users.noreply.github.com>
2021-04-02 01:40:32 +0700

    first commit

--- app/src/main/java/com/example/hoon/MainActivity.kt
+++ app/src/main/java/com/example/hoon/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.hoon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button5.setOnClickListener {
+            val it = Intent(this,Calculater::class.java)
+            startActivity(it)
+
+        }
 
     }
 }
